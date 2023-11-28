87a6092ba287c0f0e7d23b3ceac18541607b20c1
Roimer Garcia <roimergarcia@gmail.com>
2023-11-27 22:54:31 -0400

    Add item: OK and Cancel command

--- ViewModels/AddItemViewModel.cs
+++ ViewModels/AddItemViewModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.DataModel;
 
 namespace ToDoList.ViewModels
 {
     public class AddItemViewModel : ViewModelBase
     {
-        public string Description { get; set; } = String.Empty;
+        private string _description = string.Empty;
+
+        public ReactiveCommand<Unit, ToDoItem> OkCommand { get; }
+        public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+
+        public AddItemViewModel()
+        {
+            var isValidObservable = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrWhiteSpace(x));
+
+            OkCommand = ReactiveCommand.Create(
+                () => new ToDoItem { Description = Description }, isValidObservable);
+            CancelCommand = ReactiveCommand.Create(() => { });
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => this.RaiseAndSetIfChanged(ref _description, value);
+        }
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -7,20 +7,33 @@ namespace ToDoList.ViewModels
     {
         public ToDoListViewModel ToDoList { get; }
 
+        /// <summary>
+        /// Contiene el ViewModel que se mostrará en la ventana principal 
+        /// </summary>
         private ViewModelBase _contentViewModel;
 
-        public MainWindowViewModel()
-        {
-            var service = new ToDoListService();
-            ToDoList = new ToDoListViewModel(service.GetItems());
-            _contentViewModel = ToDoList;
-        }
+        /// <summary>
+        /// Devuelve\establece el viewmodel que se mostrará en la ventana principal 
+        /// </summary>
         public ViewModelBase ContentViewModel
         {
             get => _contentViewModel;
             private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
         }
 
+        /// <summary>
+        /// Al cargar: obtiene la lista ToDo actual y muestra la interface de lista.
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            var service = new ToDoListService();
+            ToDoList = new ToDoListViewModel(service.GetItems());
+            _contentViewModel = ToDoList;
+        }
+
+        /// <summary>
+        /// Muestra la interfaz "Agregar Lista" 
+        /// </summary>
         public void AddItem()
         {
             ContentViewModel = new AddItemViewModel();
